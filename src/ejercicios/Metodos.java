d06e819c87630c25cce95df34a81b3243a6c947c
Manuel De Pedro <manudpg97gmail.com>
2021-03-05 20:02:53 +0100

    Creado metodo concatenacion.

--- src/ejercicios/Metodos.java
+++ src/ejercicios/Metodos.java
@@ -3,8 +3,16 @@ package ejercicios;
 public class Metodos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println(concatenacion("hola"," que tal"));
+		
 	}
 
+	
+	 static String concatenacion (String palabra1, String palabra2) {
+		 
+		 		return palabra1 + palabra2;
+		
+		
+	}
 }
