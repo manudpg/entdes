4c740a45c323d458bd22b70f4a61d4a5361300d0
Manuel De Pedro <manudpg97gmail.com>
2021-03-06 10:09:44 +0100

    Creado metodo cuenta atras.

--- src/ejercicios/Metodos.java
+++ src/ejercicios/Metodos.java
@@ -3,32 +3,47 @@ package ejercicios;
 public class Metodos {
 
 	public static void main(String[] args) {
-		
-		System.out.println(valorDecimal('B'));
+
+		cuentaAtras(51, 5);
+
 	}
 
-	
-	 static String concatenacion (String palabra1, String palabra2) {
-		 
-		 		return palabra1 + palabra2;
-		
-		 		
+
+	static String concatenacion (String palabra1, String palabra2) {
+
+		return palabra1 + palabra2;
+
+
 	}
-	 
-	 static char caracter(int numero) {
-		 
-		 char valorCaracter = (char) numero;
-		 
-		 return valorCaracter;
-	 }
-	 
-	 
-	 
-	 static int valorDecimal (char caracter) {
-		 
-		 int valor = caracter;
-		 
-		 return valor;
-	 }
+
+	static char caracter(int numero) {
+
+		char valorCaracter = (char) numero;
+
+		return valorCaracter;
+	}
+
+
+
+	static int valorDecimal (char caracter) {
+
+		int valor = caracter;
+
+		return valor;
+	}
+
+
+	static void cuentaAtras(int origen , int restador) {
+
+
+		for (int i = origen; i >=0; i -=restador) {
+
+
+			System.out.println(i);
+
+		}
+
+
+	}
 }
 
