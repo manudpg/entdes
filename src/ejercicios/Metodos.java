639bf0c795ef3afaaae3ed82e777bed31a763ef9
Manuel De Pedro <manudpg97gmail.com>
2021-03-06 10:20:49 +0100

    Creado el metodo multiplos de 3.

--- src/ejercicios/Metodos.java
+++ src/ejercicios/Metodos.java
@@ -3,32 +3,53 @@ package ejercicios;
 public class Metodos {
 
 	public static void main(String[] args) {
-		
-		System.out.println(valorDecimal('B'));
+
+		multiplos3Hasta(100);	
+
 	}
 
-	
-	 static String concatenacion (String palabra1, String palabra2) {
-		 
-		 		return palabra1 + palabra2;
-		
-		 		
+
+	static String concatenacion (String palabra1, String palabra2) {
+
+		return palabra1 + palabra2;
+
+
 	}
-	 
-	 static char caracter(int numero) {
-		 
-		 char valorCaracter = (char) numero;
-		 
-		 return valorCaracter;
-	 }
-	 
-	 
-	 
-	 static int valorDecimal (char caracter) {
-		 
-		 int valor = caracter;
-		 
-		 return valor;
-	 }
+
+	static char caracter(int numero) {
+
+		char valorCaracter = (char) numero;
+
+		return valorCaracter;
+	}
+
+
+
+	static int valorDecimal (char caracter) {
+
+		int valor = caracter;
+
+		return valor;
+	}
+
+
+	static void multiplos3Hasta(int numero) {
+
+
+		System.out.println("Los multiplos de 3 hasta el " + numero + " son : ");
+
+		for (int i = 0; i < numero; i++) {
+
+
+			if(i % 3 == 0 ){
+
+				System.out.println(i);
+
+			}
+		}
+
+
+
+	}
 }
 
